2c41a995e97ff4b7f962dd133f7090941bb6a371
lewis1993cwm <39219383+lewis1993cwm@users.noreply.github.com>
2019-12-02 17:06:37 +0800

    v3

--- a/src/a/Test2.java
+++ a/src/a/Test2.java
@@ -2,4 +2,12 @@ package a;
 
 public class Test2 {
 
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		System.out.println("Test2.hashCode()");
+		return super.hashCode();
+		
+	}
+	
 }
